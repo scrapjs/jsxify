2b51c0621e7074d2f5e0bf7e01490ac7b4861314
Dmitry <df.creative@gmail.com>
2018-06-24 15:11:12 -0400

    Fix test impl

--- test/index.js
+++ test/index.js
@@ -5,46 +5,43 @@ var {h, render} = require('../')
 
 
 t('create/update/render methods', t => {
+	let el = document.body.appendChild(document.createElement('div'))
+
 	class X {
 		constructor(prop, children) {
-			console.log('create', prop.a)
+			// console.log('create', prop.a)
 		}
 
 		update(prop) {
 			this.text = prop.a
-			console.log('update', this.text)
+			// console.log('update', this.text)
 		}
 
 		render() {
-			console.log('render', this.text)
+			// console.log('render', this.text)
 
 			// return this.node
 			return document.createTextNode(this.text)
 		}
 
 		destroy() {
-			console.log('destroy', this.text)
+			// console.log('destroy', this.text)
 		}
 	}
 
 	// <X a=1 b=2/>
-	console.log('---')
 	let x = h(X, {a:1})
 
-	// this should create new X
-	render(x, document.body)
+	render(x, el)
+	t.equal(el.innerHTML, '1' ,'this should create new X')
 
-	console.log('---')
 	let y = h(X, {a: 3})
+	render([x, y], el)
+	t.equal(el.innerHTML, '13', 'this should create new X and update prev x')
 
-	// this should create new X and update prev x
-	render([x, y], document.body)
-
-	console.log('---')
-	let z = h(X, {a: 2})
-
-	// this should destroy prev y and update x
-	render(z, document.body)
+	let z = h(X, {a: 3})
+	render(z, el)
+	t.equal(el.innerHTML, '3', 'this should destroy prev y and update x')
 
 	t.end()
 })
